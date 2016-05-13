f9cd5f84efb1457f46c36b24349ab16a6c04ddcd
Kelly Farley <farleykc@gmail.com>
2016-05-13 11:58:17 -0500

    pseudocoding completed

--- pseudocode.rb
+++ pseudocode.rb
@@ -1,3 +1,81 @@
 #pseudocode logic & break down problems
 #plan sequence of events & coding
 #start coding and testing functionality in terminal
+
+#GOALS
+#sentence variety
+#word choice (giving suggestions for stronger vocabulary or showing which words are used often)
+#ending sentences in prepositions (this should be easy)
+#passive voice (this should be relatively easy)
+#repetition (although sometimes repetition can be good, it is usually boring)
+#use of second person (if an essay is submitted...on this note, maybe different editing options based on what genre of piece was submitted)
+
+#What genre is your poem?
+	#Poem
+	#Formal essay
+	#Prose
+
+#If the genre is poem, check for:
+	#word choice
+		#start off by checking for:
+			#honestly
+			#absolutely
+			#very
+			#went
+			#get
+			#really
+			#literally
+			#stuff
+			#things
+	#passive voice (but passive voice isn't always passive; if you follow it with a noun, it's ok, but if you follow it by a word ending in -ing)
+		#was
+		#were
+		#is
+	#repetition (if a word shows up too many times, not sure how to program this one)
+		#ignore common words: I, a, the, that
+
+#If the genre is formal essay, check for:
+	#use of second person
+		#check for: you, your, we, us
+	#sentence variety
+		#look at word after punctuation
+		#look at sentence length
+	#ending sentences in prepositions
+		#if preposition is before period/exclaimation mark
+	#passive voice (but passive voice isn't always passive; if you follow it with a noun, it's ok, but if you follow it by a word ending in -ing)
+		#was
+		#were
+		#is
+	#word choice
+		#start off by checking for:
+			#honestly
+			#absolutely
+			#very
+			#went
+			#get
+			#really
+			#literally
+			#stuff
+			#things
+
+#If the genre is prose, check for:
+	#sentence variety
+		#look at word after punctuation
+		#look at sentence length
+	#word choice
+		#start off by checking for:
+			#honestly
+			#absolutely
+			#very
+			#went
+			#get
+			#really
+			#literally
+			#stuff
+			#things
+	#passive voice (but passive voice isn't always passive; if you follow it with a noun, it's ok, but if you follow it by a word ending in -ing)
+		#was
+		#were
+		#is
+	#repetition (if a word shows up too many times, not sure how to program this one)
+		#ignore common words: I, a, the, that
